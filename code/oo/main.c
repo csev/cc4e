c760ed4db89091da33bed71146b6688c1a78beb3
Charles Severance <csev@umich.edu>
2019-12-28 11:02:58 -0500

    Updates - add a return value

--- code/oo/main.c
+++ code/oo/main.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include "point.c"
 
 int main(void)
 {
     struct point * p3 = point_new(4,5);
-    p3->print(p3);
+    p3->str(p3);
+    printf("Added=%d\n",p3->add(p3));
     p3->del(p3);
 }
 
-/* https://stackoverflow.com/questions/12642830/can-i-define-a-function-inside-a-c-structure/12642878#12642878 */
 
--- code/oo/point.c
+++ code/oo/point.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* https://stackoverflow.com/questions/12642830/can-i-define-a-function-inside-a-c-structure/12642878#12642878 */
-
 struct point
 {
     int x;
     int y;
-    void (*print)(const struct point*);
+    int (*add)(const struct point*);
+
+    void (*str)(const struct point*);
     void (*del)(const struct point*);
 };
 
-void point_print(const struct point* self)
+void point_str(const struct point* self)
 {
     printf("x=%d\n", self->x);
     printf("y=%d\n", self->y);
+}
+
+int point_add(const struct point* self)
+{
+    return (self->x + self->y);
 }
 
 void point_del(const struct point* self) {
@@ -25,8 +30,11 @@ struct point * point_new(int x, int y) {
     struct point *p = malloc(sizeof(*p));
     p->x = x;
     p->y = y;
-    p->print = point_print;
+    p->add = point_add;
+    p->str = point_str;
     p->del = point_del;
     return p;
 }
 
+/* https://stackoverflow.com/a/59509251/1994792 */
+/* https://stackoverflow.com/questions/12642830/can-i-define-a-function-inside-a-c-structure/12642878#12642878 */
