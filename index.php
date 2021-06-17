6c4c6fbe33e9a69ff4cde0fd2be5ccc5adce6927
Charles Severance <csev@umich.edu>
2021-06-17 12:07:10 -0400

    Add security

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+if ( isset($_POST['secret']) && $_POST['secret'] == '42' ) {
+    setCookie('secret', '42', time() + 15 * 3600 * 24);
+} else if ( !isset($_COOKIE['secret']) || $_COOKIE['secret'] != '42' ) {
+?>
+<form method="post">
+Enter the unlock code
+<input type="text" name="secret">
+<input type="submit">
+</form>
+<?php
+    return;
+}
+?>
+<h1>Welcome</h1>
+<p>
+Here is the 
+<a href="md/chap01.md">book in progress</a>.
--- md/index.php
+++ md/index.php
@@ -2,6 +2,11 @@
 
 require_once "Parsedown.php";
 
+if ( !isset($_COOKIE['secret']) || $_COOKIE['secret'] != '42' ) {
+    header("Location: ../index.php");
+    return;
+}
+
 if ( ! function_exists('endsWith') ) {
 function endsWith($haystack, $needle) {
     // search forward starting from end minus needle length characters
@@ -51,6 +56,7 @@ center {
 
 if ( $contents != false ) {
 ?>
+<script src="https://static.tsugi.org/js/jquery-1.11.3.js"></script>
 <script>
 function onSelect() {
     console.log($('#chapters').val());
@@ -59,10 +65,14 @@ function onSelect() {
 </script>
 <div style="float:right">
 <select id="chapters" onchange="onSelect();">
-  <option <?= x_sel("ngrok_mac.md") ?>>Using Ngrok on the Mac</option>
-  <option <?= x_sel("ngrok_win.md") ?>>Using Ngrok on Windows</option>
-  <option <?= x_sel("lt_mac.md") ?>>Using LocalTunnel on Mac</option>
-  <option <?= x_sel("lt_win.md") ?>>Using LocalTunnel on Win-10</option>
+  <option <?= x_sel("chap01.md") ?>>Chapter 1</option>
+  <option <?= x_sel("chap02.md") ?>>Chapter 2</option>
+  <option <?= x_sel("chap03.md") ?>>Chapter 3</option>
+  <option <?= x_sel("chap04.md") ?>>Chapter 4</option>
+  <option <?= x_sel("chap05.md") ?>>Chapter 5</option>
+  <option <?= x_sel("chap06.md") ?>>Chapter 6</option>
+  <option <?= x_sel("chap07.md") ?>>Chapter 7</option>
+  <option <?= x_sel("chap08.md") ?>>Chapter 8</option>
 </select>
 </div>
 <?php
@@ -71,21 +81,11 @@ function onSelect() {
 } else {
 ?>
 <p>
-This is a set of supplementary documentation for use with this
-web site.
+This is a work in progress start here: 
+<a href="chap01.md">Chapter 1</a>,
 </p>
-<ul>
-<li><a href="ngrok_mac.md">Using Ngrok and the Autograder on a Macintosh</a></li>
-<li><a href="ngrok_win.md">Using Ngrok and the Autograder on Windows-10</a></li>
-<li><a href="lt_mac.md">Using LocalTunnel and the Autograder on a Macintosh</a></li>
-<li><a href="lt_win.md">Using LocalTunnel and the Autograder on Windows-10</a></li>
-</ul>
-<p>
-Note that if you are running Windows and the bash shell, NGrok does not seem to start.  
-Use the Windows Command line to run NGrok on Windows.
-<p>
-If you find a mistake in this documentation, feel free to send me a fix using
-<a href="https://github.com/csev/dj4e/tree/main/md" target="_blank">Github</a>.
+Please feel free to improve this text in 
+<a href="https://github.com/csev/cc4e/tree/main/md" target="_blank">Github</a>.
 </p>
 <?php
 }
