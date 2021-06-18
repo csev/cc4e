33ce91fe5dc5cd25e9df773710a2df7e27873d97
Charles Severance <csev@umich.edu>
2021-06-18 11:51:24 -0400

    Add the first code bit.

---
+++ book/code/c_161_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define BUFSIZE	512 /* best size for PDP-11 UNIX */
+
+main() /* copy input to output */
+{
+char buf[BUFSIZE];
+int n;
+while ((n = read(0, buf, BUFSIZE)) > 0)
+    write(1, buf, n);
+}
+
