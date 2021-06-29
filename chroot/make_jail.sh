de96f27acbb2bcb11ef4f57e4b1d9117cbda9783
Charles R. Severance <csev@umich.edu>
2021-06-29 21:17:05 +0000

    Updates

--- chroot/make_jail.sh
+++ chroot/make_jail.sh
@@ -8,22 +8,17 @@ cp hello.c jail
 
 cd jail
 
-./simple-chroot.sh install bash vim gcc as ld ls which rm find 
+./simple-chroot.sh install busybox bash sh vim gcc as ld ls which rm find
 
-# mkdir tmp
-# chmod 1777 tmp
-# mkdir var
-# mkdir var/tmp
-# chmod 1777 var/tmp
+mkdir usr/libexec 
+cp -r /usr/libexec/* usr/libexec
 
-# mkdir usr/lib/gcc/
-# mkdir usr/lib/gcc/x86_64-linux-gnu
-# cp -r /usr/lib/gcc/x86_64-linux-gnu/* usr/lib/gcc/x86_64-linux-gnu
-# cp -r /usr/lib/x86_64-linux-gnu/* usr/lib/x86_64-linux-gnu
+mkdir usr/include
+cp -r /usr/include/* usr/include
 
-# mkdir usr/include
-# mkdir usr/include/x86_64-linux-gnu
-# cp -r /usr/include/x86_64-linux-gnu/* usr/include/x86_64-linux-gnu
-# cp -r /usr/include/* usr/include
-# cp -r /lib/x86_64-linux-gnu/* lib/x86_64-linux-gnu/
+mkdir usr/lib
+cp -r /usr/lib/* usr/lib
+
+mkdir lib
+cp -r /lib/* lib
 
