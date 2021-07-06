168dc42a471510d220c578b28d4396df7efe75c9
yashajoshi <55447190+yashajoshi@users.noreply.github.com>
2021-07-06 17:48:15 -0400

    Chapter 03 edits

---
+++ book/code/c_002_02.c
@@ -0,0 +1,16 @@
+#define MAXLINE 1000
+
+main() /* remove trailing blanks and tabs */
+{
+  int n;
+  char line[MAXLINE];
+
+  while ((n = getline(line, MAXLINE)) > 0) {
+    while (--n >= 0)
+      if (line[n] != ' ' && line[n] != '\t'
+        && line[n] != '\n')
+          break;
+    line[n+1] = '\0';
+    printf("%s\n", line);
+  }
+}
