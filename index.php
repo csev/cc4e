55599560fb62fd4e44dd115789bc441cd0bf106b
Charles Severance <csev@umich.edu>
2021-07-10 12:56:08 -0400

    Update index.

--- index.php
+++ index.php
@@ -12,7 +12,11 @@ if ( isset($_POST['secret']) && $_POST['secret'] == '42' ) {
 <input type="text" name="secret">
 <input type="submit" value="Unlock">
 <p>
-The unlock code is a number.
+The unlock code is a number.  You won't be given the number.  It is a puzzle - 
+to prove you have the pre-requisite skills for this course - you need to figure
+the number out yourself.  There are clues in this page and the links from this page.
+</p>
+<p>
 It is not too long and has none of those funny hex (abcde) characters.
 It is a very significant number with that
 makes a regular appearance throughout Dr. Chuck's other online courses
@@ -34,6 +38,10 @@ You can view the
 We take your privacy seriously.
 </p>
 </form>
+    <script language="javascript">
+    console.log('The code is a number that is central to the book "Hitchhiker\'s Guide to the Galaxy');
+    console.log('It is also the number of Dr. Chuck\'s race car');
+    </script>
 </center>
 <?php
     return;
