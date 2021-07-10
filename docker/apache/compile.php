90d0db7bad56552fde46a4882873fc2376bbaae5
Charles R. Severance <csev@umich.edu>
2021-07-10 17:34:16 +0000

    MVP

--- docker/apache/compile.php
+++ docker/apache/compile.php
@@ -1,6 +1,7 @@
 <?php
 
-$ip = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['REMOTE_ADDR'] ?? false;
+if ( ! is_string($ip) ) die('No REMOTE_ADDR');
 
 $public = filter_var(
     $ip, 
@@ -12,9 +13,84 @@ if ( is_string($public) ) {
 	die('bad address');
 }
 
-$content = isset($_POST['content']) ? $_POST['content'] : false;
-if ( ! is_string($content) ) die('No content');
+$code = $_POST['code'] ?? false;
+$input = $_POST['input'] ?? false;
+if ( ! is_string($code) ) die('No code');
 
-$json = json_decode($content);
-if ( ! is_object($json)) die ('Bad JSON');
-die('Yada');
+$folder = sys_get_temp_dir() . '/compile-' . md5(uniqid());
+mkdir($folder);
+
+$retval = new \stdClass();
+$retval->code = $code;
+$retval->input = $input;
+$retval->folder = $folder;
+$retval->compile_status = false;
+$retval->compile_out = false;
+$retval->compile_err = false;
+$retval->run_status = false;
+$retval->run_out = false;
+$retval->run_err = false;
+
+
+$descriptorspec = array(
+   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+   2 => array("pipe", "w") // stderr is a file to write to
+);
+
+$cwd = $folder;
+$env = array(
+    'some_option' => 'aeiou',
+    'PATH' => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+);
+
+$command = 'gcc -ansi -x c -o a.out -';
+$process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
+
+if (is_resource($process)) {
+    // $pipes now looks like this:
+    // 0 => writeable handle connected to child stdin
+    // 1 => readable handle connected to child stdout
+    // Any error output will be appended to /tmp/error-output.txt
+
+    fwrite($pipes[0], $code);
+    fclose($pipes[0]);
+
+    $retval->compile_out = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    $retval->compile_err = stream_get_contents($pipes[2]);
+    fclose($pipes[2]);
+
+    // It is important that you close any pipes before calling
+    // proc_close in order to avoid a deadlock
+    $retval->compile_status = proc_close($process);
+
+}
+
+if ( $retval->compile_status === 0 ) {
+
+$command = './a.out';
+$process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
+
+if (is_resource($process)) {
+    // $pipes now looks like this:
+    // 0 => writeable handle connected to child stdin
+    // 1 => readable handle connected to child stdout
+    // Any error output will be appended to /tmp/error-output.txt
+
+    fwrite($pipes[0], $input);
+    fclose($pipes[0]);
+
+    $retval->run_out = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    $retval->run_err = stream_get_contents($pipes[2]);
+    fclose($pipes[2]);
+
+    // It is important that you close any pipes before calling
+    // proc_close in order to avoid a deadlock
+    $retval->run_status = proc_close($process);
+
+}
+
+header("Content-type: application/json; charset=utf-8");
+echo(json_encode($retval, JSON_PRETTY_PRINT));
