ff143865e97f67bf2ebce0a7a0fd182cf4a962a8
Charles Severance <csev@umich.edu>
2021-07-13 00:39:44 -0400

    Updates

--- sandbox/compile.php
+++ sandbox/compile.php
@@ -39,9 +39,10 @@ $input = $_POST['input'] ?? false;
 if ( ! is_string($code) ) die('No code');
 
 $folder = sys_get_temp_dir() . '/compile-' . md5(uniqid());
+$now = str_replace('@', 'T', gmdate("Y-m-d@H:i:s"));
 $folder = '/tmp/compile';
 if ( file_exists($folder) ) {
-    system('rm $folder/*');
+    system("rm -rf $folder/*");
 } else {
     mkdir($folder);
 }
@@ -52,6 +53,7 @@ $env = array(
 
 $retval = new \stdClass();
 $retval->code = $code;
+$retval->now = $now;
 $retval->input = $input;
 $retval->folder = $folder;
 
@@ -64,10 +66,24 @@ $retval->docker = false;
 $allowed = false;
 
 if ( $pipe1->status === 0 ) {
-    // $assembly = file_get_contents($folder . '/student.s');
     $assembly = $retval->assembly->stdout;
     $lines = explode("\n", $assembly);
     $newlines = array();
+
+    // Make a symbol table
+    $symbol = array();
+    foreach ( $lines as $line) {
+        $matches = array();
+        if ( ! preg_match('/^(_[a-zA-Z0-9_]+):/', $line, $matches ) ) continue;
+        if ( count($matches) > 1 ) $symbol[] = $matches[1];
+    }
+    $retval->symbol = $symbol;
+
+    $legit = array(
+        'puts', 'printf', 'putchar'
+    );
+
+    $externals = array();
     foreach ( $lines as $line) {
         if ( strlen($line) < 1                 // blank lines
             || (! preg_match('/^\s/', $line))  // _main: 
@@ -77,6 +93,7 @@ if ( $pipe1->status === 0 ) {
             $new[] = $line;
             continue;
         }
+
         // These should be the remaining executable statements
         // Linux:
         // 	call	puts@PLT
@@ -92,10 +109,25 @@ if ( $pipe1->status === 0 ) {
         //  leaq	L_.str(%rip), %rdi
         //  leaq	_fun(%rip), %rax
 
-        // echo($line."\n");
+        $matches = array();
+        if ( preg_match('/^([a-zA-Z0-9_]+)@PLT/', $line, $matches) ) {
+            if ( count($matches) > 1 ) $externals[] = $matches[1];
+        }
+
+        $matches = array();
+        if ( preg_match('/^([a-zA-Z0-9_]+)@GOTPCREL/', $line, $matches) ) {
+            if ( count($matches) > 1 ) $externals[] = $matches[1];
+        }
+
         $pieces = explode("\t", $line);
         // var_dump($pieces);
+        if ( count($pieces) > 2 ) {
+            if ( $pieces[1] == 'callq' ) {
+                $externals[] = $pieces[2];
+            }
+        }
     }
+    $retval->externals = $externals;
     $allowed = true;
 }
 
