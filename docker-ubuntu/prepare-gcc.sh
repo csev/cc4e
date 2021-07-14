b6add35cb54317a763d9c30345a98faa73384be8
Charles Severance <csev@umich.edu>
2021-07-13 22:29:26 -0400

    Updates

--- docker-ubuntu/prepare-gcc.sh
+++ docker-ubuntu/prepare-gcc.sh
@@ -8,4 +8,14 @@ cd /var/www
 mv html /tmp
 git clone https://github.com/csev/cc4e.git www
 
+echo ======= Cleanup Start
+df
+apt-get -y autoclean
+apt-get -y clean
+apt-get -y autoremove
+rm -rf /var/lib/apt/lists/*
+echo ======= Cleanup Done
+df
+echo ======= Cleanup Done
+
 echo "---- Preparation complete ----"
