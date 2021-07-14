a4dbce24f99ba6b6cc96278ddfe269a7835be3b7
Charles Severance <csev@umich.edu>
2021-07-13 22:53:39 -0400

    Updates

--- docker-ubuntu/configure-gcc.sh
+++ docker-ubuntu/configure-gcc.sh
@@ -2,20 +2,13 @@
 COMPLETE=/usr/local/bin/gcc-complete
 
 if [ -f "$COMPLETE" ]; then
-    echo "gcc jail has already has run"
+    echo "gcc setup has already has run"
 else
 
-echo "---- Starting gcc jail setup  ----"
-
-cd /home/gcc/cc4e
+cd /var/www/html
 git pull
 
-cd /home/gcc/cc4e/chroot
-bash make_jail.sh
-
-cp -r /home/gcc/cc4e/docker/apache/* /var/www/html
-
-echo "---- Finished gcc jail setup  ----"
+echo "---- Finished gcc setup  ----"
 
 # if COMPLETE
 fi
