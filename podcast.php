e80c29ac623e6f2858e8745dbdc8deaa3b3e1c6c
Charles Severance <csev@umich.edu>
2021-07-14 12:12:08 -0400

    Tweak

--- podcast.php
+++ podcast.php
@@ -1,6 +1,6 @@
 <?php include("top.php");?>
 <?php include("nav.php");?>
-<h2>Lectures Podcast
+<h2>Lectures 
 (<a href="https://audio.cc4e.com/" target="_blank">Subscribe</a>)
 </h2>
 <div id='buzzsprout-small-player-1813198'></div><script type='text/javascript' charset='utf-8' src='https://www.buzzsprout.com/1813198.js?container_id=buzzsprout-small-player-1813198&player=small'></script>
