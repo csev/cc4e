fc0401cf1470b93a62dd5734737e4edd0b33ba79
Charles Severance <csev@umich.edu>
2022-06-21 08:54:51 -0400

    Updates

--- book/code/c_081_01.c
+++ book/code/c_081_01.c
@@ -4,9 +4,9 @@
  *
  * to compile on a Unix style system use the command:
  *
- * gcc -S n_081_01.c
+ * gcc -S c_081_01.c
  *
- * and compare the contents of the n_081_01.s files with and without
+ * and compare the contents of the c_081_01.s files with and without
  * the register keyword.
  */
 
@@ -18,7 +18,7 @@ int main() {
     scanf("%d", &compute);
     printf("compute %d\n", compute);
     for(iter=0; iter<1000; iter++) {
-        compute = (compute * 22) * 7;
+        compute = (compute * 22) / 7;
         if ( compute > 1000 ) compute = compute % 1000;
     }   
     printf("compute %d\n",compute);
