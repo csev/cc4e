bee1fa5ae02c0fa0627adc7706cd653a41e866bb
Charles Severance <csev@umich.edu>
2022-07-09 23:37:08 -0400

    Updates

--- play.php
+++ play.php
@@ -54,7 +54,7 @@ cc4e_play_header($lines);
 <body>
 <p>
 This is the <a href="index.php">www.cc4e.com</a> code playground for writing C programs
-It is a very limited environment - you can find a number of more full-featureed
+It is a very limited environment - you can find a number of more full-featured
 C compilers <a href="compilers.php">online</a>.
 <?php
 if ( U::get($_REQUEST, "sample", null) != null ) {
