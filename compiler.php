2eca2c7c6a261b12eb8e1c1d12a33de17f877e16
Charles Severance <csev@umich.edu>
2022-07-09 23:29:10 -0400

    Updates

---
+++ compiler.php
@@ -0,0 +1,21 @@
+<h1> Free online C compilers</h1>
+<p>
+These are free online C-Compilers available on other sites.
+</p>
+<ul>
+<li><a href="https://www.pythonanywhere.com/">
+PythonAnywhere - Use the Shell
+</a></li>
+<li><a href="https://www.tutorialspoint.com/compile_c_online.php">
+https://www.tutorialspoint.com/compile_c_online.php
+</a></li>
+<li><a href="https://www.programiz.com/c-programming/online-compiler/">
+https://www.programiz.com/c-programming/online-compiler/
+</a></li>
+<li><a href="https://onecompiler.com/c">
+https://onecompiler.com/c
+</a></li>
+<li><a href="https://replit.com/languages/c">
+https://replit.com/languages/c
+</a></li>
+</ul>
