0c1f8cf43bfb9aa15b389f03ff93d51c9bade6a7
cahan0506 <ch0506@hotmail.com>
2022-07-21 09:42:29 +0200

    Update c_167_01.c

--- book/code/c_167_01.c
+++ book/code/c_167_01.c
@@ -55,25 +55,25 @@ register char *name, *mode;
   for (fp = _iob; fp < _iob + _NFILE; fp++)
     if ((fp->_flag & (_READ | _WRITE)) == 0)
       break; /* found free slot */
-    if (fp >= _iob + _NFILE) /* no free slots */
-      return(NULL);
+  if (fp >= _iob + _NFILE) /* no free slots */
+    return(NULL);
 
-    if (*mode == 'w') /* access file */
+  if (*mode == 'w') /* access file */
+    fd = creat(name, PMODE);
+  else if (*mode == 'a') {
+    if ((fd = open(name, 1)) == -1)
       fd = creat(name, PMODE);
-    else if (*mode == 'a') {
-      if ((fd = open(name, 1)) == -1)
-        fd = creat(name, PMODE);
-      lseek(fd, 0L, 2);
-    } else
-      fd = open (name, 0);
-    if (fd == -1) /* couldn't access name */
-      return(NULL);
-    fp->_fd = fd;
-    fp->_cnt = 0;
-    fp->_base = NULL;
-    fp->_flag &= ~( _READ | _WRITE);
-    fp->_flag |= (*mode == 'r') ? _READ : _WRITE;
-    return(fp);
+    lseek(fd, 0L, 2);
+  } else
+    fd = open (name, 0);
+  if (fd == -1) /* couldn't access name */
+    return(NULL);
+  fp->_fd = fd;
+  fp->_cnt = 0;
+  fp->_base = NULL;
+  fp->_flag &= ~( _READ | _WRITE);
+  fp->_flag |= (*mode == 'r') ? _READ : _WRITE;
+  return(fp);
 }
 
 /* Sample code from page 168, merged for compilation */
