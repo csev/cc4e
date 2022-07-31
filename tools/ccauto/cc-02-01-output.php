480b9dccfbcdd99b5ad4c8453dcfec26d8b98bd1
Charles Severance <csev@umich.edu>
2022-07-30 20:25:45 -0400

    Updates

--- tools/ccauto/cc-02-01-output.php
+++ tools/ccauto/cc-02-01-output.php
@@ -7,14 +7,17 @@ use \Tsugi\Util\U;
 use \Tsugi\Util\Mersenne_Twister;
 
 // Called first
-function ccauto_instructions($LAUNCH) { return <<< EOF
+function ccauto_instructions($LAUNCH) { 
+    GLOBAL $RANDOM_CODE_HOUR, $INT_2_1;
+    $INT_2_1 = ($RANDOM_CODE_HOUR % 5) + 15;
+    return <<< EOF
 <b>Exercise cc-2-1:</b> Write a C program to produce the same output as
 this Python program:
 <pre>
 print('Hello world')
 print('Answer', 42)
 print('Name', 'Sarah')
-print('x',3.5,'i',10)
+print('x',3.5,'i',$INT_2_1)
 </pre>
 EOF
 ;
@@ -33,23 +36,27 @@ EOF
 ;
 } 
 
-function ccauto_solution($LAUNCH) { return <<< EOF
+function ccauto_solution($LAUNCH) { 
+    GLOBAL $RANDOM_CODE_HOUR, $INT_2_1;
+    return <<< EOF
 #include <stdio.h>
 int main() {
     printf("Hello world\\n");
     printf("Answer %d\\n", 42);
     printf("Name %s\\n", "Sarah");
-    printf("x %.1f i %d\\n", 3.5, 100);
+    printf("x %.1f i %d\\n", 3.5, $INT_2_1);
 }
 EOF
 ;
 }
 
-function ccauto_output($LAUNCH) { return <<< EOF
+function ccauto_output($LAUNCH) { 
+    GLOBAL $RANDOM_CODE_HOUR, $INT_2_1;
+    return <<< EOF
 Hello world
 Answer 42
 Name Sarah
-x 3.5 i 100
+x 3.5 i $INT_2_1
 EOF
 ;
 }
