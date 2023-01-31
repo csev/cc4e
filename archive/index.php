87833f610119ef3e7cdf5aa59a9de7de956d0bfe
Charles Severance <csev@umich.edu>
2023-01-31 12:30:22 -0500

    Updates

---
+++ archive/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+</head>
+<body>
+<img src="1990-Chuck-Members-Only-Jacket.png" style="float:right; width:240px;"/>
+<h1>Archives of Courses Taught by Dr. Chuck</h1>
+<p>These are related courses taught by Dr. Chuck in the past.</p>
+<ul>
+<li><p><a href="1991-lbs290">Lyman Briggs (LBS290) - C</a>
+Taught Fall 1991 at Michigan State University.  This was taught with students
+logging in to a 
+<a href="https://en.wikipedia.org/wiki/3B_series_computers#3B2" target="_blank">AT&T 3B2</a>
+mincomputer.
+</p></li>
+<li><p><a href="1992-lbs290f" target="_blank">Lyman Briggs (LBS290F) - FORTRAN</a><br/>
+Taught Winter 1992 at Michgan State University.
+</p></li>
+<li><p>
+<a href="2000-eecs280" target="_blank">EECS 280 - C++</a><br/>
+Taught Winter 2000 at the University of Michigan.  This was the first
+and only time that Dr. Chuck taught C++ and the very first course
+Dr. Chuck taught at the University of Michigan.
+</p>
+</li>
+</ul>
