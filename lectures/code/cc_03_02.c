3480ab0bad3457d64f8d6edb351703127c9f58a6
Charles Severance <csev@umich.edu>
2023-02-27 08:14:09 -0500

    Updates.

--- lectures/code/cc_03_02.c
+++ lectures/code/cc_03_02.c
@@ -8,38 +8,44 @@ struct point
     double y;
 };
 
-void point_dump(const struct point* self)
+struct PointStruct {
+    double x;
+    double y;
+
+    struct PointStruct * (*new)(double x, double y);
+    void (*del)(const struct PointStruct* self);
+    void (*dump)(const struct PointStruct* self);
+    double (*origin)(const struct PointStruct* self);
+};
+
+void point_dump(const struct PointStruct* self)
 {
     printf("Object point@%p x=%f y=%f\n", 
             self, self->x, self->y);
 }
 
-void point_del(const struct point* self) {
+void point_del(const struct PointStruct* self) {
   free((void *)self);
 }
 
-double point_origin(const struct point* self) {
+double point_origin(const struct PointStruct* self) {
     return sqrt(self->x*self->x + self->y*self->y);
 }
 
-struct point * point_new(double x, double y) {
-    struct point *p = malloc(sizeof(*p));
+struct PointStruct * point_new(double x, double y) {
+    struct PointStruct *p = malloc(sizeof(*p));
     p->x = x;
     p->y = y;
+    p->dump = &point_dump;
+    p->new = &point_new;
+    p->origin = &point_origin;
+    p->del = &point_del;
     return p;
 }
-
-struct PointStruct {
-    struct point * (*new)(double x, double y);
-    void (*del)(const struct point* self);
-    void (*dump)(const struct point* self);
-    double (*origin)(const struct point* self);
-};
 
 struct PointStruct PointClass;
 
 void import_PointClass() {
-    /* Build the Point class */
     PointClass.dump = &point_dump;
     PointClass.new = &point_new;
     PointClass.origin = &point_origin;
@@ -50,9 +56,9 @@ int main(void)
 {
     import_PointClass();
 
-    struct point * pt = PointClass.new(4.0,5.0);
+    struct PointStruct * pt = PointClass.new(4.0,5.0);
     PointClass.dump(pt);
-    printf("Origin %f\n", PointClass.origin(pt));
+    printf("Origin %f\n", pt->origin(pt));
     PointClass.del(pt);
 }
 
--- lectures/code/cc_03_03.py
+++ lectures/code/cc_03_03.py
@@ -1,18 +1,25 @@
 import math
 
 class PointClass:
+
+    # Class variables
+    label = "point"
+
+    # Instance variables
     def __init__(self, x, y):
         self.x = x
         self.y = y
         
     def dump(self):
-        print('Object point@%x x=%f y=%f' % (id(self),self.x,self.y))
+        print('Object %s@%x x=%f y=%f' % (self.label, id(self),self.x,self.y))
 
     def origin(self):
         return math.sqrt(self.x*self.x+self.y*self.y)
         
 pt = PointClass(4.0, 5.0)
+
 PointClass.dump(pt)
 print('Origin',pt.origin())
+
 del(pt)
 
