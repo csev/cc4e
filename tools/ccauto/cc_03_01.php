b2ce63269845980b7a77b75809573101ddb5ab79
Charles Severance <csev@umich.edu>
2023-03-01 14:50:16 -0500

    Updates

---
+++ tools/ccauto/cc_03_01.php
@@ -0,0 +1,5 @@
+Make doubly linked list of integers ListClass - add, find, delete in the style of StructList
+with method pointers.
+
+Keep it sorted...
+
---
+++ tools/ccauto/cc_04_01.php
@@ -0,0 +1,12 @@
+Make doubly linked dictionary with a string keys pointing to integers - 
+put, get, delete in the style of StructDict with method pointers.
+
+02
+Sort by keys
+
+03
+Sort by values (descending)
+
+04
+Most common word
+
