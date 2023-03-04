4bb75d4eb94495463b6316ad0aa4b881f1480487
Charles Severance <csev@umich.edu>
2023-03-04 12:57:43 -0500

    Updates

--- lectures/code/cc_03_04.py
+++ lectures/code/cc_03_04.py
@@ -16,10 +16,12 @@ print("z=", dct.get("z", 404))
 print("x=", dct.get("x", 404))
 
 print("Sort by key");
-print(dict(sorted(dct.items())))
+dct = dict(sorted(dct.items()))
+print(dct)
 
 print("Sort by value");
-print(dict(sorted(dct.items(), key=lambda x: x[1])))
+dct = dict(sorted(dct.items(), key=lambda x: x[1]))
+print(dct)
 
 print("\nDump")
 for key in dct:
