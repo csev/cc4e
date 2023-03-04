9d707372af5bd306a1f6b8cc50baf9d05b1c6983
Charles Severance <csev@umich.edu>
2023-03-04 15:33:45 -0500

    Updates.

--- lectures/code/cc_03_04.py
+++ lectures/code/cc_03_04.py
@@ -1,7 +1,4 @@
-import collections
-
 dct = dict();
-
 dct["z"] = "Catch phrase"
 print(dct);
 dct["z"] = "W"
@@ -11,20 +8,8 @@ dct["c"] = "C"
 dct["a"] = "D"
 print(dct);
 print("Length =", len(dct));
-
 print("z=", dct.get("z", 404))
 print("x=", dct.get("x", 404))
-
-print("Sort by key");
-dct = dict(sorted(dct.items()))
-print(dct)
-
-print("Sort by value");
-dct = dict(sorted(dct.items(), key=lambda x: x[1]))
-print(dct)
-
 print("\nDump")
 for key in dct:
     print(key+"="+dct[key])
-
-
