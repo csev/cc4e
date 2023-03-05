6d498ca5a9bf611d71fe77af1128044421ba678b
Charles R. Severance <csev@umich.edu>
2023-03-05 05:04:42 +0000

    Updates

--- tools/ccauto/cc_03_01-pystr.php
+++ tools/ccauto/cc_03_01-pystr.php
@@ -13,7 +13,12 @@ function ccauto_instructions($LAUNCH) {
 <p>
     You will a set of supporting functions using the following structure
     to implement a subset of the Python str class in C.  Some code and
-    method signatures have been prodived for you as well as a main() program.
+    method signatures have been provided for you as well as a main() program
+    and some support routines from the lecture slides.  In the constructor,
+    you will allocate a 10 character buffer.  If as data is added, it exceeds
+    the length of the buffer use realloc() to expand the buffer by 10.
+    There is a good deal of discussion of this application in the lecture
+    materials associtate with this assignment.
 <pre>
 struct pystr
 {
@@ -30,9 +35,24 @@ EOF
 // Make sure to escape \n as \\n
 function ccauto_sample($LAUNCH) {
     return <<< EOF
-void yada() {
- printf("Yada\\n");
+/* x = x + 'h'; */
+
+void pystr_append(struct pystr* self, char ch) {
+    /* Need about 10 lines of code here */
 }
+
+/* x = x + "hello"; */
+
+void pystr_appends(struct pystr* self, char *str) {
+    /* Need a line or two of code here */
+}
+
+/* x = "hello"; */
+
+void pystr_assign(struct pystr* self, char *str) {
+    /* Need about three lines of code here */
+}
+
 EOF
 ;
 }
@@ -62,14 +82,14 @@ struct pystr * pystr_new() {
 
 /* Destructor - del(x) */
 void pystr_del(const struct pystr* self) {
-  free((void *)self->data); /* free string first */
-  free((void *)self);
+    free((void *)self->data); /* free string first */
+    free((void *)self);
 }
 
 void pystr_dump(const struct pystr* self)
 {
-    printf("Object pystr@%p length=%d alloc=%d data=%p data=%s\\n",
-            self, self->length, self->alloc, self->data, self->data);
+    printf("Pystr length=%d alloc=%d data=%s\\n",
+            self->length, self->alloc, self->data);
 }
 
 int pystr_len(const struct pystr* self)
@@ -82,34 +102,6 @@ char *pystr_str(const struct pystr* self)
     return self->data;
 }
 
-/* x = x + 'h'; */
-void pystr_append(struct pystr* self, char ch) {
-    /* If we don't have space for 1 character plus
-       termination, allocate 10 more */
-
-    if ( self->length >= (self->alloc - 2) ) {
-        self->alloc = self->alloc + 10;
-        self->data = (char *) realloc(self->data, self->alloc);
-    }
-
-    /* Add our character to the end and terminate */
-    self->data[self->length] = ch;
-    self->length = self->length + 1;    
-    self->data[self->length] = '\\0';
-}
-
-/* x = x + "hello"; */
-void pystr_appends(struct pystr* self, char *str) {
-    char *s;
-    for(s = str; *s; s++) pystr_append(self, *s);
-}
-
-/* x = "hello"; */
-void pystr_assign(struct pystr* self, char *str) {
-    self->length = 0;
-    self->data[0] = '\\0';
-    pystr_appends(self, str);
-}
 
 int main(void)
 {
@@ -137,18 +129,11 @@ function ccauto_input($LAUNCH) { return false; }
 function ccauto_output($LAUNCH) { 
     GLOBAL $RANDOM_CODE_HOUR, $CHAR_2_10, $LOWER_2_10;
     return <<< EOF
-Dump:
-  10
-  20
-  30
-Did not find 42
-Found 30
-
-Dump:
-  10
-  20
-  30
-  40
+Pystr length=0 alloc=10 data=
+Pystr length=1 alloc=10 data=H
+Pystr length=11 alloc=20 data=Hello world
+String = A completely new string
+Length = 23
 EOF
 ;
 }
@@ -171,48 +156,6 @@ function ccauto_require($LAUNCH) {
 // Make sure to escape \n as \\n
 function ccauto_solution($LAUNCH) {
     return <<< EOF
-#include <stdio.h>
-#include <stdlib.h>
-
-struct pystr
-{
-    int length;
-    int alloc; /* the length of *data */
-    char *data;
-};
-
-/* Constructor - x = str() */
-struct pystr * pystr_new() {
-    struct pystr *p = malloc(sizeof(*p));
-    p->length = 0;
-    p->alloc = 10;
-    p->data = malloc(10);
-    p->data[0] = '\\0';
-    return p;
-}
-
-/* Destructor - del(x) */
-void pystr_del(const struct pystr* self) {
-  free((void *)self->data); /* free string first */
-  free((void *)self);
-}
-
-void pystr_dump(const struct pystr* self)
-{
-    printf("Object pystr@%p length=%d alloc=%d data=%p data=%s\\n",
-            self, self->length, self->alloc, self->data, self->data);
-}
-
-int pystr_len(const struct pystr* self)
-{
-    return self->length;
-}
-
-char *pystr_str(const struct pystr* self)
-{
-    return self->data;
-}
-
 /* x = x + 'h'; */
 void pystr_append(struct pystr* self, char ch) {
     /* If we don't have space for 1 character plus
@@ -241,25 +184,6 @@ void pystr_assign(struct pystr* self, char *str) {
     self->data[0] = '\\0';
     pystr_appends(self, str);
 }
-
-int main()
-{
-    struct pystr * x = pystr_new();
-    pystr_dump(x);
-
-    pystr_append(x, 'H');
-    pystr_dump(x);
-
-    pystr_appends(x, "ello world");
-    pystr_dump(x);
-
-    pystr_assign(x, "A completely new string");
-    printf("String = %s\\n", pystr_str(x));
-    printf("Length = %d\\n", pystr_len(x));
-    pystr_del(x);
-}
-
-// rm -f a.out ; gcc pystr.c; a.out ; rm -f a.out
 EOF
 ;
 }
