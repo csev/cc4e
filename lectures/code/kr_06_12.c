15eb824aab2548de3b6cb5471aebbcca658ab6a5
Charles Severance <csev@umich.edu>
2023-03-06 07:55:41 -0500

    Updates.

--- lectures/code/kr_06_12.c
+++ lectures/code/kr_06_12.c
@@ -1,32 +1,8 @@
 #include <stdio.h>
 
+/* Note to self - use mask and shift instead of union fields */
+
 int main() {
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    union format {
-        char ch;
-        struct {
-            unsigned bottom : 6;
-            unsigned opcode : 2;
-        } inst;
-    } ;
-#else
-    union format {
-        char ch;
-        struct {
-            unsigned opcode : 2;
-            unsigned bottom : 6;
-        } inst;
-    } ;
-#endif
-
-    unsigned int i, op;
-    union format bits;
-
-    while(scanf("%x", &i) == 1 ) {
-        bits.ch = i;
-        op = bits.inst.opcode;
-        printf("%x %d\n", bits.ch, op);
-        if ( op == 2 || op == 3 ) printf("opcode %d\n",op);
-        else printf("%c\n", bits.ch);
-    }
+    char c = 0xF1;
+    printf("%d\n", (c & 0xC0) >> 6);
 }
