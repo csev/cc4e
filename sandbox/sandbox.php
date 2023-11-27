1315924561ec7610eba0c99242962edc8eb094ca
Charles R. Severance <csev@umich.edu>
2023-11-27 17:33:29 +0000

    Add another warning override

--- sandbox/sandbox.php
+++ sandbox/sandbox.php
@@ -263,7 +263,7 @@ function cc4e_compile_internal($code, $input, $main=null)
 
     $retval->folder = $folder;
 
-    $gcc_options = '-ansi -Wno-return-type -Wno-pointer-to-int-cast -Wno-builtin-declaration-mismatch -Wno-int-conversion';
+    $gcc_options = '-ansi -Wno-return-type -Wno-pointer-to-int-cast -Wno-builtin-declaration-mismatch -Wno-int-conversion -Wno-deprecated-declarations';
     $gcc_options = U::get($CFG->extensions, 'cc4e_gcc_options', $gcc_options);
 
     $command = 'rm -rf * ; cat > student.c ; gcc '.$gcc_options.' -fno-asm -S student.c ; [ -f student.s ] && cat student.s';
@@ -416,7 +416,7 @@ function cc4e_compile_internal($code, $input, $main=null)
 
     $eof = 'EOF' . md5(uniqid());
     if ( $retval->hasmain && $allowed && $minimum && ! $retval->reject ) {
-        $gcc_options = '-ansi -Wno-return-type -Wno-pointer-to-int-cast -Wno-builtin-declaration-mismatch -Wno-int-conversion';
+        $gcc_options = '-ansi -Wno-return-type -Wno-pointer-to-int-cast -Wno-builtin-declaration-mismatch -Wno-int-conversion -Wno-deprecated-declarations';
         $gcc_options = U::get($CFG->extensions, 'cc4e_gcc_options', $gcc_options);
 
         $script = "cd /tmp;\n";
