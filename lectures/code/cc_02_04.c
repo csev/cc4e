15eaf7505f907a2cb96612babe11c3979a9b658d
Charles Severance <csev@umich.edu>
2024-06-06 08:44:48 -0400

    Fix scanf and array size for issue #97

--- lectures/code/cc_02_04.c
+++ lectures/code/cc_02_04.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
-    char line[101];
+    char line[1001];
     printf("Enter line\n");
-    scanf("%100[^\n]s", line);
+    scanf("%1000[^\n]s", line);
     printf("Line: %s\n", line);
 }
 
