3af8e4341c190efe1e1ba9f24aa9bdcba375931d
Charles Severance <csev@umich.edu>
2026-04-24 16:40:12 -0400

    Updates

--- buildmenu.php
+++ buildmenu.php
@@ -3,10 +3,12 @@
 use \Tsugi\Util\U;
 
 function buildMenu() {
-    global $CFG;
+    global $CFG, $USER;
     $R = $CFG->apphome . '/';
     $T = $CFG->wwwroot . '/';
     $adminmenu = isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true";
+    $isInstructor = (isset($USER) && $USER && isset($USER->instructor) && $USER->instructor)
+        || (isset($_SESSION['instructor']) && $_SESSION['instructor']);
     $showCalendarDueUi = isset($_SESSION['id'])
         && U::isNotEmpty($CFG->lessons)
         && \Tsugi\Grades\GradeUtil::showDueDates(U::get($_SESSION, 'context_id', 0));
@@ -28,18 +30,23 @@ function buildMenu() {
         $submenu->addLink('EmScriptEn', "https://emscripten.org");
         $set->addLeft('Write C', $submenu);
         $submenu = new \Tsugi\UI\Menu();
-        $submenu->addLink('Profile', $R.'profile');
+        $submenu->addLink('Announcements', $R.'announcements');
+        $submenu->addLink('Grades', $R.'grades');
+        $submenu->addLink('Pages', $R.'pages');
+        $submenu->addLink('Discussions', $R.'discussions');
+        if ( $isInstructor ) {
+            $submenu->addLink('Notifications', $R.'notifications');
+        }
+        $submenu->addLink('Courses', $R.'coursesredirect.php');
         if ( isset($CFG->google_map_api_key) ) {
             $submenu->addLink('Map', $R.'map');
         }
-    
+        $submenu->addLink('Profile', $R.'profile');
+        if ( $showCalendarDueUi ) {
+            $submenu->addLink('Calendar', $R.'calendar');
+        }
         // $submenu->addLink('Badges', $R.'badges');
         $submenu->addLink('Materials', $R.'materials');
-        $submenu->addLink('Announcements', $R.'announcements');
-        $submenu->addLink('Notifications', $R.'notifications');
-        $submenu->addLink('Grades', $R.'grades');
-        $submenu->addLink('Pages', $R.'pages');
-        $submenu->addLink('Courses', $R.'coursesredirect.php');
         $submenu->addLink('LMS Integration', $T . 'settings');
 
         if ( isset($CFG->google_classroom_secret) ) {
@@ -67,7 +74,12 @@ function buildMenu() {
     }
 
     if ( isset($_SESSION['id']) ) {
-        $set->addRight('<tsugi-notifications api-url="'. htmlspecialchars($T . 'api/notifications.php') . '" notifications-view-url="'. htmlspecialchars($R . 'notifications') . '" announcements-view-url="'. htmlspecialchars($R . 'announcements') . '"></tsugi-notifications>', false);
+        $set->addRight(
+            '<tsugi-notifications api-url="'. htmlspecialchars($T . 'api/notifications.php') . '" notifications-view-url="'. htmlspecialchars($R . 'notifications') . '" announcements-view-url="'. htmlspecialchars($R . 'announcements') . '"></tsugi-notifications>',
+            false,
+            true,
+            'hidden-xs tsugi-wc-nav-item'
+        );
         if ( $showCalendarDueUi ) {
             $set->addRight(
                 '<tsugi-calendar-due api-url="'. htmlspecialchars($R . 'calendar/json') . '" lessons-url="'. htmlspecialchars($R . 'lessons') . '"></tsugi-calendar-due>',
